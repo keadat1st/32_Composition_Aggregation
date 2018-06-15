90d318e752cff8ba768139350c3bd7d96ce405a2
Claus Bové <clbo@kea.dk>
2018-06-15 16:02:45 +0200

    Create Book.java

---
+++ improved_code/composition/Book.java
@@ -0,0 +1,16 @@
+// Java program to illustrate 
+// the concept of Composition
+ 
+class Book 
+{
+ 
+    public String title;
+    public String author;
+     
+    Book(String title, String author)
+    {
+         
+        this.title = title;
+        this.author = author;
+    }
+}
