057b15a952443b3f2e7aa40b248163fbfb52b63d
Claus Bové <clbo@kea.dk>
2018-06-15 16:25:22 +0200

    Update GFG.java

--- improved_code/composition/GFG.java
+++ improved_code/composition/GFG.java
@@ -8,12 +8,12 @@ class GFG
     public static void main (String[] args) 
     {
          
-        // Creating the Objects of Book class.
+        // Creating 3 new books
         Book b1 = new Book("EffectiveJ Java", "Joshua Bloch");
         Book b2 = new Book("Thinking in Java", "Bruce Eckel");
         Book b3 = new Book("Java: The Complete Reference", "Herbert Schildt");
          
-        // Creating a Library and adding the 3 books to it.
+        // Creating a Library (new Library()) and adding the 3 books to it.
         Library library = new Library();
         library.addBook(b1);
         library.addBook(b2);
