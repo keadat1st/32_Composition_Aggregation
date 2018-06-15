0b83bacaac46f1006ec11a80de443302e140adbc
Claus Bové <clbo@kea.dk>
2018-06-15 14:54:44 +0200

    Update Department.java

--- improved_code/aggregation/Department.java
+++ improved_code/aggregation/Department.java
@@ -1,1 +1,19 @@
-
+/* Department class contains list of student
+Objects. It is associated with student
+class through its Object(s). */
+class Department 
+{
+    String name;
+    private ArrayList<Student> students;
+  
+    Department(String name, ArrayList<Student> students) 
+    {     
+        this.name = name;
+        this.students = students;    
+    }
+     
+    public ArrayList<Student> getStudents() 
+    {
+        return students;
+    }
+}
