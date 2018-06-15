130d7b290795d5caa4cd4b81cb29eeda94255911
Claus Bové <clbo@kea.dk>
2018-06-16 00:12:23 +0200

    Update Driver.java

--- code_from_teachings/Driver.java
+++ code_from_teachings/Driver.java
@@ -3,12 +3,13 @@ public class Driver {
    private String name;
    private String licenseNumber;
         
-   public Driver(String name){
+   public Driver(String name, String licenseNumber ){
       this.name = name;
+      this.licenseNumber = licenseNumber;
    }
    
    public String getDriver(){
-         return name;
+         return name + ", " + licenseNumber;
    }
    
    public String toString(){
