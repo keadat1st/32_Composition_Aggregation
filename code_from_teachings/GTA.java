c582aa483de80ced6106b793a26c6a80bdd7154e
Claus Bové <clbo@kea.dk>
2018-06-16 00:14:44 +0200

    Update GTA.java

--- code_from_teachings/GTA.java
+++ code_from_teachings/GTA.java
@@ -1,31 +1,11 @@
 public class GTA {
  
-   private static Driver driver = new Driver("Kenneth");
-   private static Car bmw = new Car(driver);
-
+   
    public static void main(String[] args){
 
+      Driver driver = new Driver("Kenneth");
+      Car bmw = new Car(driver);
+
       System.out.println(bmw.getDriver());
-      
-      
-      
-      
-      
-      
-      
-      
-      /*Driver[] drivers = bmw.getDrivers();
-      
-      for(int i = 0; i <= 1; i++){
-         System.out.println(drivers[i].getDriver());
-      }*/
-      
-      //System.out.println(bmw.getDrivers());
-      
-     
-   
-   
    }
-
-
 }
